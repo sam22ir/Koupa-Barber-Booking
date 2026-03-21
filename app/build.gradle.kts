0a4e6b2b2acad8769f7c2fc99060e3a853530a6a
sam22ir <saadisamir20200@gmail.com>
2026-03-21 03:38:14 +0100

    Fix: Add kotlinx.serialization plugin + rotated app icons
    - Added kotlinx.serialization plugin to build.gradle.kts
    - Added kotlinx-serialization-json:1.6.2 dependency
    - Fixed app icon rotation (recreated all mipmap icons)
    - Fixes app crash on launch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.dagger.hilt.android")
     id("com.google.gms.google-services")
     id("com.google.devtools.ksp")
@@ -102,6 +103,9 @@ dependencies {
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+
+    // Kotlinx Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 
     // Coil for image loading
     implementation("io.coil-kt:coil-compose:2.5.0")
